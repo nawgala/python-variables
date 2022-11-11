79bef73b3400b6b89a01139468b7f4932bc82c2a
sampath <sampathr@hsenidmobile.com>
2022-11-11 10:46:28 +0530

    Add SetSample.py to code base

---
+++ SetSample.py
@@ -0,0 +1,8 @@
+fruits = {1, 2,3,3}
+print(fruits)
+print(type(fruits))
+# print(fruits[0]) TypeError  : 'set' does not support indexing
+print(len(fruits))
+vegetable = set(("beet", "leeks", "carrot"))
+print(vegetable)
+print(type(vegetable))
